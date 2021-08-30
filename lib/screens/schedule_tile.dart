9f26fc98a97ec677ecec96339b8007dd8da043f4
usman <usmanarshad5051@outlook.com>
2021-08-31 01:48:39 +0500

    time update

--- lib/screens/schedule_tile.dart
+++ lib/screens/schedule_tile.dart
@@ -2,12 +2,21 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-class ScheduleTile extends StatelessWidget {
-  String time = "6:00 PM";
-  TextStyle myStyle = TextStyle(color: Color(0xE8D2D2D4));
+class ScheduleTile extends StatefulWidget {
   int index;
+  TimeOfDay time = TimeOfDay.now();
+  TimeOfDay? selectedTime;
   StreamController tempController;
   ScheduleTile(this.index, this.tempController);
+
+  @override
+  State<ScheduleTile> createState() => _ScheduleTileState();
+}
+
+class _ScheduleTileState extends State<ScheduleTile> {
+
+
+  TextStyle myStyle = TextStyle(color: Color(0xE8D2D2D4));
 
   @override
   Widget build(BuildContext context) {
@@ -15,22 +24,33 @@ class ScheduleTile extends StatelessWidget {
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-        Text("Start time", style: myStyle,),
+        GestureDetector(
+          onTap: (){
+              selectTime(context);
+          },
+            child: Text("Start time", style: myStyle,)),
         Text("End time", style: myStyle,)
       ],),
 
       subtitle: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-        Text(time),
-        Text(time)],),
+        Text("${widget.time.hour} : ${widget.time.minute} "),
+        Text(widget.time.hour.toString())],),
 
       trailing: GestureDetector(
           onTap: (){
-            tempController.sink.add(index);
+            widget.tempController.sink.add(widget.index);
           },
           child: Icon(Icons.remove_circle, color: Colors.red,)),
 
     );
+  }
+
+  Future<void> selectTime(BuildContext context) async {
+    widget.selectedTime = await showTimePicker(context: context, initialTime: widget.time);
+    setState(() {
+    widget.time = widget.selectedTime!;
+    });
   }
 }
